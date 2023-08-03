97044d69b01ca77c9f4d46a4a10e406a3c512450
杨浩冰 <3195399130@qq.com>
2023-08-03 15:32:55 +0800

    提交

---
+++ vue项目/练习/123.vue
@@ -0,0 +1,27 @@
+<!--  -->
+<template>
+<div class="">
+
+</div>
+</template>
+
+<script>
+export default {
+    name:"",
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+<style lang="less" scoped>
+/* @import url(); 引入css类 */
+
+</style>
